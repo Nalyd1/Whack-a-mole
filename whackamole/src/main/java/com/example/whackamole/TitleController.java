81873e21ec5bd6589e85c1cfaa5f135c0ba69fbd
Nalyd1 <susanjohnson27330@outlook.com>
2023-11-22 14:07:04 -0500

    Cleaned directory

--- whackamole/src/main/java/com/example/whackamole/TitleController.java
+++ whackamole/src/main/java/com/example/whackamole/TitleController.java
@@ -3,33 +3,46 @@ package com.example.whackamole;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.fxml.Initializable;
 
 import java.io.IOException;
 
-public class TitleController {
-    private Stage stage;
+public class TitleController{
     private Scene scene;
-    private Parent root;
+    private Stage stage;
+
     @FXML
-    private void startButtonPressed(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("whackamolePlayScreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    private Label titleLab1;
+    @FXML
+    private Button startButton;
+    @FXML
+    private GridPane scoreBoardGridPane;
+
+    public TitleController(Stage stage){
+        this.stage = stage;
+    }
+
+    public void switchToPlayScreen(ActionEvent event) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Whack_a_mole.class.getResource("whackamolePlayScreen.fxml"));
+        PlayScreenController playScreenController = new PlayScreenController();
+        fxmlLoader.setController(playScreenController);
+        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = FXMLLoader.load(getClass().getResource("whackamolePlayScreen.fxml"));
         stage.setScene(scene);
         stage.show();
     }
-
-    @FXML
-    private void playButtonPressed(ActionEvent event) throws IOException{
-        root = FXMLLoader.load(getClass().getResource("whackamoleGameScreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    public void switchToTitleSceen(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("whackamoletitle.fxml"));
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 }
-
