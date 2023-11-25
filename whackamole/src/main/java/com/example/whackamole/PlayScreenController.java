e87c9528f6bf80bec4332e904af15e458aa04535
Connor <jch3008@uncw.edu>
2023-11-25 17:45:06 -0500

    Added stage to PlayScreenController, setup a new action event handler to switch to the game from play screen.

--- whackamole/src/main/java/com/example/whackamole/PlayScreenController.java
+++ whackamole/src/main/java/com/example/whackamole/PlayScreenController.java
@@ -1,21 +1,34 @@
 package com.example.whackamole;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class PlayScreenController implements Initializable {
+    public PlayScreenController(Stage stage){
+        this.stage = stage;
+    }
+    private Stage stage;
+
     @FXML
     private Label label;
     @FXML
     private VBox theVbox;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
+
 
         // makes 3 radio buttons uses to pick the users difficulty
         RadioButton button1 = new RadioButton("Hard");
@@ -37,6 +50,19 @@ public class PlayScreenController implements Initializable {
         // the vbox holds the buttons
         theVbox.getChildren().addAll(button1, button2, button3);
 
+
+    }
+    public void switchGameScreen(ActionEvent event) throws IOException { System.out.println("switchingToGameScreen");
+        FXMLLoader fxmlLoader = new FXMLLoader(Whack_a_mole.class.getResource("whackamoleGameScreen.fxml"));
+        gameController gameController = new gameController(stage);
+        fxmlLoader.setController(gameController);
+        Parent root = fxmlLoader.load();
+        System.out.println("FXML Loaded");
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("Scene set");
 
     }
 }
